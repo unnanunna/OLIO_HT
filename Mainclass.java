143c9fb2ebdfd3fa3f70f2b274d39e4fb56b80d8
MiisaSuorsa <82520902+MiisaSuorsa@users.noreply.github.com>
2021-04-20 17:40:52 +0300

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -10,19 +10,35 @@ public class Mainclass {
 		double inputExercise1;
 		int exercise;
 		
-		WaterTracker Tracker = new WaterTracker();
+		String name = "";
+		int age = 0;
+		float weight = 0;
+		String gender = "";
+		float exercise1 = 0;
+		//private float exercise;
 		
-		System.out.print("Your weight: ");
+		WaterTracker Tracker = new WaterTracker();
+		Entry entry = new Entry();
+		
+		entry.getName(name);
+		entry.getAge(age);
+		weight = entry.getWeight(weight);
+		entry.getGender(gender);
+		exercise1 = entry.getExercise(exercise1);
+		
+		/*System.out.print("Your weight: ");
 		Scanner scanner1 = new Scanner(System.in);
 		inputWeight = scanner1.nextDouble();
 		
 		System.out.print("Your exercise per day (hours): ");
 		Scanner scanner2 = new Scanner(System.in);
-		inputExercise1 = scanner2.nextDouble();
-		//muutetaan minuuteiksi ja pyˆristet‰‰n int
-		exercise = (int) Math.round(inputExercise1*60);
+		inputExercise1 = scanner2.nextDouble();*/
+		
+		//muutetaan minuuteiksi ja py√∂ristet√§√§n int
+		exercise = (int) Math.round(exercise1*60);
 
-		System.out.println("Result: " + Tracker.waterAmount(inputWeight, exercise));
+		//System.out.println("Result: " + Tracker.waterAmount(inputWeight, exercise));
+		System.out.println("Result: " + Tracker.waterAmount(weight, exercise));
 		System.out.println("End.");
 	}
 }
