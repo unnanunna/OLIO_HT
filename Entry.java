1d9e30c22b53f6e9efa04444f432e012528de266
MiisaSuorsa <82520902+MiisaSuorsa@users.noreply.github.com>
2021-04-20 17:43:18 +0300

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -1,7 +1,50 @@
 package HT;
-
+import java.util.Scanner;
 public class Entry {
 	
-	private Date date;
+	//private Date date;
+	
+	//User luokassa olevat muutujat
+	private String name;
+	private int age;
+	private float weight;
+	private String gender;
+	private float exercise;
+	
 
+	public String getName(String name) {
+		System.out.print("Your name: ");
+		Scanner scanner1 = new Scanner(System.in);
+		name = scanner1.nextLine();
+		return name;
+	}
+	
+	public int getAge(int age) {
+		System.out.print("Your age: ");
+		Scanner scanner2 = new Scanner(System.in);
+		age = scanner2.nextInt();
+		return age;
+	}
+	
+	public float getWeight(float weight) {
+		System.out.print("Your weight: ");
+		Scanner scanner3 = new Scanner(System.in);
+		weight = scanner3.nextFloat();
+		return weight;
+	}
+	
+	public String getGender(String gender) {
+		System.out.print("Your gender: ");
+		Scanner scanner4 = new Scanner(System.in);
+		gender = scanner4.nextLine();
+		return gender;
+	}
+	
+	public float getExercise(float exercise) {
+		System.out.print("Your exercise per day (hours): ");
+		Scanner scanner3 = new Scanner(System.in);
+		exercise = scanner3.nextFloat();
+		return exercise;
+	}
 }
+
