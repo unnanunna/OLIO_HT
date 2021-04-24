e1658d8582dda40c7b8543af44d93ca63bd533e7
MiisaSuorsa <82520902+MiisaSuorsa@users.noreply.github.com>
2021-04-24 12:13:24 +0300

    Update VitaminRecomment.java

--- VitaminRecomment.java
+++ VitaminRecomment.java
@@ -2,8 +2,19 @@ package HT;
 
 public class VitaminRecomment {
 	
-	public String recommenterVitamin(String gender, int age) {
+	private int vitamins = 0;
+	
+	/*public String recommenterVitamin(String gender, int age) {
 		return lause;
+	}*/
+	
+	public void vitaminAdd() {
+		if (vitamins == 0) {
+			vitamins += 1;
+			System.out.println("Congratulations, you have taken your vitamins today.");
+		}
+		else if (vitamins == 1) {
+			System.out.println("You already had your vitamins today.");
+		}
 	}
-
 }
