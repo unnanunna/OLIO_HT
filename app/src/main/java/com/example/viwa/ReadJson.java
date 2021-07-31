d7a365b6d17b25fa2736bebcf0d882761aeb9c5f
unnanunna <82447901+unnanunna@users.noreply.github.com>
2021-07-31 20:42:40 +0300

    Viwa

--- app/src/main/java/com/example/viwa/ReadJson.java
+++ app/src/main/java/com/example/viwa/ReadJson.java
@@ -1,107 +1,45 @@
 package com.example.viwa;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.View;
-import android.widget.Button;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 
 
-public class ReadJson {
+public class ReadJson{
+
+    static ReadJson instance = new ReadJson();
 
 
-        /*StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        //this.getJson();
-
-        final Button Button = (Button) findViewById(R.id.button);
-        Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View t) {
-                /*try {
-                    readJson();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    public static ReadJson getInstance() {
+        return instance;
+    }
 
 
-    }*/
-
-
-    /*public void readJson() throws JSONException {
-        String json = getJson();
-        //System.out.println("JSON: " + json);
-        String jsonTemp = "";
+    //Reads Json file and searches the right information from it to show at the info page
+    public String read(String jsonText) throws JSONException {
+        String json = jsonText;
+        String finalText = "";
+        String id = "";
 
         if (json != null) {
             try {
-                JSONObject root = new JSONObject(json);
-                JSONObject data = root.getJSONObject("description");
+                JSONArray root = new JSONArray(json);
 
-                for (int i = 0; i < data.length(); i++) {
-                    //JSONObject dataentry = (JSONObject) data.get(1);
-                    //JSONObject data1 = dataentry.getJSONObject("description");
-                    jsonTemp = data.getString("en");
-                    System.out.println(jsonTemp);
+                for (int i=0; i<root.length(); i++){
+                    JSONObject data = root.getJSONObject(i);
+                    id = data.getString("id");
+
+                    if (id.equals("2271")){
+                        JSONObject dataentry2 = data.getJSONObject("description");
+                        finalText = dataentry2.getString("fi");
+                    }
                 }
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-    }*/
-
-    /*public String getJson() {
-        String response = null;
-        try {
-            URL url = new URL("https://fineli.fi/fineli/api/v1/components/");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            //HttpClient client = HttpClients.custom().build();
-            /*HttpUriRequest request = RequestBuilder.get().setUri(SAMPLE_URL).setHeader(HttpHeaders.CONTENT_TYPE, "application/json").build();
-            client.execute(request);*/
-            /*HttpClient client = HttpClients.custom().build();
-            HttpUriRequest request = RequestBuilder.get().setUri("https://fineli.fi/fineli/api/v1/components/").setHeader(HttpHeaders.description, "application/json").build();
-            client.execute(request);*/
-
-            /*InputStream input = new BufferedInputStream(connection.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(input));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            response = sb.toString();
-            input.close();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return response;
-    }*/
-
+        return finalText;
+    }
 }
-
-
-
-
